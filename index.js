56a3d3a430745bdf1f067cc7fc958a0cd2f7f577
Jon-Stickrod <jonstickrodCodes@gmail.com>
2020-02-16 17:20:43 -0800

    14 out of 25

--- index.js
+++ index.js
@@ -135,8 +135,9 @@ function processProduct(num1, num2, callback) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
-  /* CODE HERE */
+function processContains(item, list, callback) {
+  var n = list.includes(item);
+  return callback(n);
 }
 
 /**
@@ -180,8 +181,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+ let names = [];
+  runners.forEach(function(runners){
+    return names.push(`${runners.last_name}, ${runners.first_name}`) 
+  });
+  return names;
 }
 
 /**
@@ -196,8 +201,14 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let names = [];
+   runners.map(
+    function (name) {
+      return names.push(name.first_name.toUpperCase())
+    }
+  )
+    return names;
 }
 
 /**
@@ -213,8 +224,9 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  var filter = runners.filter(runners => tShirtSize == runners.shirt_size);
+  return filter;
 }
 
 /**
